957bac42223775c38120906894aad60e707b6930
TI-PraiaSol <143218068+TI-PraiaSol@users.noreply.github.com>
2026-02-03 17:07:34 -0300

    v0.7.2
    Atualizado métodos de verificação do token

--- src/middlewares/RoleGuard.ts
+++ src/middlewares/RoleGuard.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import Resp from "../utils/Response";
+import { NextFunction, Request, Response as ExpressResponse } from "express";
+import Response, { StandardResponse } from "../utils/Response";
+import { JwtPayload } from "./Authentication";
 
 export const authorize = (allowedTypes: string[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const user = res.locals.user;
+  return (req: Request, res: ExpressResponse, next: NextFunction) => {
+    const user = res.locals.user as JwtPayload;
 
     if (!user || !allowedTypes.includes(user.userType)) {
-      const resp = Resp.unauthorized("Você não tem permissão para acessar este recurso.");
+      const resp = Response.forbidden(`Tipo de usuário não permitido. Necessário: ${allowedTypes.join(' ou ')}`);
       return res.status(resp.status).json({ message: resp.message });
     }
 
